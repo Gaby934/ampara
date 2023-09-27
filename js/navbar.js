4fd99bae75ca914a8f8d8a0667125d879f24db3b
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-09-27 20:37:40 -0300

    menu

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,66 @@ function includeNavbar(){
     });
 }
 
+var itensDoMenu = document.getElementsByClassName('nav-item');
+for(var z = 0; z < itensDoMenu.length; z++) {
+    var elem = itensDoMenu[z];   
+    var nomeDoMenu = elem.innerHTML
+    if(nomeDoMenu=='Home'){
+      elem.onclick = function() {
+        fetch("../pages/home.html")
+        .then((response) => response.text())
+        .then((data) => {
+          document.getElementById("conteudo-site").innerHTML = data;
+        });
+
+          return false;
+      };
+    }
+    if(nomeDoMenu=='Resgate'){
+      elem.onclick = function() {
+        fetch("../pages/resgate.html")
+        .then((response) => response.text())
+        .then((data) => {
+          document.getElementById("conteudo-site").innerHTML = data;
+        });
+
+          return false;
+      };
+    }
+    if(nomeDoMenu=='Contato'){
+      elem.onclick = function() {
+        fetch("../pages/contato.html")
+        .then((response) => response.text())
+        .then((data) => {
+          document.getElementById("conteudo-site").innerHTML = data;
+        });
+
+          return false;
+      };
+    }
+    if(nomeDoMenu=='Sobre Nós'){
+      elem.onclick = function() {
+        fetch("../pages/sobre-nos.html")
+        .then((response) => response.text())
+        .then((data) => {
+          document.getElementById("conteudo-site").innerHTML = data;
+        });
+
+          return false;
+      };
+    }
+    if(nomeDoMenu=='Doe'){
+      elem.onclick = function() {
+        fetch("../pages/doe.html")
+        .then((response) => response.text())
+        .then((data) => {
+          document.getElementById("conteudo-site").innerHTML = data;
+        });
+
+          return false;
+      };
+    }
+    
+    
+}
+
